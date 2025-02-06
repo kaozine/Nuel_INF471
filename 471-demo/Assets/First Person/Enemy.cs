2595187632be94f4e3fe489dadd8a2da84e8c0f9
Kaosi Nuel <142933817+kaozine@users.noreply.github.com>
2025-02-06 01:19:44 -0500

    in class fps

---
+++ 471-demo/Assets/First Person/Enemy.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    int health = 5;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnTriggerEnter()
+    {
+        health -= 1;
+    }
+}
